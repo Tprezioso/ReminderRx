e1875d2e98e5ed415d1b357a401c8224bef07ffb
Thomas <tommyprezioso@gmail.com>
2021-11-09 20:52:11 -0500

    Initial Commit

---
+++ ReminderRx/ReminderRxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderRxApp.swift
+//  ReminderRx
+//
+//  Created by Thomas Prezioso Jr on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ReminderRxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
