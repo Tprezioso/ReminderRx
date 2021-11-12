8998379f28f92e6dc67f66df9d7eae34ae406fa0
Thomas <tommyprezioso@gmail.com>
2021-11-11 19:26:15 -0500

    super basic layout setup

---
+++ ReminderRx/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  ReminderRx
+//
+//  Created by Thomas Prezioso Jr on 11/9/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var exampleArray = [
+        Prescription(name: "Drug 1", count: 99),
+        Prescription(name: "Drug 2", count: 10),
+        Prescription(name: "Drug 3", count: 12),
+        Prescription(name: "Drug 4", count: 56),
+        Prescription(name: "Drug 5", count: 34),
+        Prescription(name: "Drug 6", count: 2)
+
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List(exampleArray) { example in
+                Text(example.name)
+            }.listStyle(PlainListStyle())
+            .navigationTitle("Reminder RX")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
+struct Prescription: Identifiable {
+    let id = UUID()
+    var name = ""
+    var count = 0
+    
+}
--- ReminderRx/ReminderRxApp.swift
+++ ReminderRx/ReminderRxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ReminderRxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
