dab2d71825c00254279b4278c6475c8ea7bbb99f
Thomas <tommyprezioso@gmail.com>
2022-03-17 21:56:12 -0400

    added circular progress

---
+++ ReminderRx/Views/CustomProgressView.swift
@@ -0,0 +1,41 @@
+//
+//  CustomProgressView.swift
+//  ReminderRx
+//
+//  Created by Thomas Prezioso Jr on 3/17/22.
+//
+
+import SwiftUI
+
+struct CustomProgressView: View {
+    @Binding var progress: CGFloat
+    
+    var body: some View {
+        ZStack {
+            // placeholder
+            Circle()
+                .stroke(lineWidth: 10)
+                .foregroundColor(.gray)
+                .opacity(0.2)
+            
+            // progress circle
+            Circle()
+                .trim(from: 0.0, to: min(progress, 1.0))
+                .stroke(AngularGradient(colors: [.red, .orange, .yellow, .green], center: .center), style: StrokeStyle(lineWidth: 10, lineCap: .butt, lineJoin: .miter))
+                .rotationEffect(.degrees(-90))
+                .shadow(radius: 2)
+            
+            Text("\(String(format: "%0.0f", progress * 100))")
+            
+        }
+        .frame(width: 50, height: 50)
+        .padding()
+        .animation(.easeInOut, value: progress)
+    }
+}
+
+struct CustomProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomProgressView(progress: .constant(0.9))
+    }
+}
